c0b95960fe9dcb1161bb2c197022b63f4a028a0e
Hugo <hugo@yourcloudtrainer.com>
2021-06-03 17:13:01 +0000

    Adding 2 vms to serve as Master and Worker of our user cluster

--- setup_vxlan.sh
+++ setup_vxlan.sh
@@ -5,6 +5,8 @@ ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
 bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW1_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
 ip addr add 10.200.0.3/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -15,6 +17,8 @@ ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
 bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW1_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
 ip addr add 10.200.0.4/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -25,6 +29,8 @@ ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
 bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
 ip addr add 10.200.0.5/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -35,6 +41,32 @@ ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
 bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
 ip addr add 10.200.0.6/24 dev vxlan0
+ip link set up dev vxlan0
+'
+
+gcloud compute instances add-metadata user-cluster-master0 --zone $ZONE \
+--metadata startup-script='set -x
+ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
+bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AW1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
+ip addr add 10.200.0.7/24 dev vxlan0
+ip link set up dev vxlan0
+'
+
+gcloud compute instances add-metadata user-cluster-worker0 --zone $ZONE \
+--metadata startup-script='set -x
+ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
+bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AW1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+ip addr add 10.200.0.8/24 dev vxlan0
 ip link set up dev vxlan0
 '
--- user_desktop_vxlan.sh.bak
+++ user_desktop_vxlan.sh.bak
@@ -3,5 +3,7 @@ sudo bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 sudo bridge fdb append to 00:00:00:00:00:00 dst <AW1_IP> dev vxlan0
 sudo bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
 sudo bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
+sudo bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+sudo bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
 sudo ip addr add 10.200.0.2/24 dev vxlan0
 sudo ip link set up dev vxlan0
