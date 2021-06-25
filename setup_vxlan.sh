8db53aff889b17e847a8cc3461b7c2fb196f1528
Hugo <hugo@yourcloudtrainer.com>
2021-06-25 16:20:33 +0000

    Setup private ips for cluster nodes and add user-desktop vxlan config from the beginning

--- setup_vxlan.sh
+++ setup_vxlan.sh
@@ -1,3 +1,15 @@
+gcloud compute instances add-metadata user-desktop --zone $ZONE \
+--metadata startup-script='set -x
+ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
+bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
+ip addr add 10.200.0.2/24 dev vxlan0
+ip link set up dev vxlan0
+'
+
 gcloud compute instances add-metadata admin-workstation --zone $ZONE \
 --metadata startup-script='set -x
 ip link add vxlan0 type vxlan id 42 dev ens4 dstport 4789
@@ -5,6 +17,7 @@ bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UD_IP> dev vxlan0
 ip addr add 10.200.0.3/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -16,6 +29,7 @@ bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UD_IP> dev vxlan0
 ip addr add 10.200.0.4/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -27,6 +41,7 @@ bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UD_IP> dev vxlan0
 ip addr add 10.200.0.5/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -38,6 +53,7 @@ bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC1_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UD_IP> dev vxlan0
 ip addr add 10.200.0.6/24 dev vxlan0
 ip link set up dev vxlan0
 '
@@ -49,6 +65,7 @@ bridge fdb append to 00:00:00:00:00:00 dst <AM_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AW0_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <AWS_IP> dev vxlan0
 bridge fdb append to 00:00:00:00:00:00 dst <UC0_IP> dev vxlan0
+bridge fdb append to 00:00:00:00:00:00 dst <UD_IP> dev vxlan0
 ip addr add 10.200.0.7/24 dev vxlan0
 ip link set up dev vxlan0
 '
